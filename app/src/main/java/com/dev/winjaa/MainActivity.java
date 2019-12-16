a188da0bec00e7a340b9969f57e954338d1a928c
Mirou <amarhamlaoui@gmail.com>
2019-12-16 04:46:11 +0100

    Added a WebClient!

--- app/src/main/java/com/dev/winjaa/MainActivity.java
+++ app/src/main/java/com/dev/winjaa/MainActivity.java
@@ -2,20 +2,40 @@ package com.dev.winjaa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    private WebView webView;
+    String appCachePath = getApplicationContext().getCacheDir().getAbsolutePath();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        WebView webView = findViewById(R.id.web);
+        webView = findViewById(R.id.web);
         webView.setWebViewClient(new WebViewClient());
-        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAppCachePath( getApplicationContext().getCacheDir().getAbsolutePath() );
+        webView.getSettings().setAllowFileAccess( true );
+        webView.getSettings().setAppCacheEnabled( true );
+        webView.getSettings().setJavaScriptEnabled( true );
+        webView.getSettings().setCacheMode( WebSettings.LOAD_DEFAULT );
+        webView.setWebViewClient(new MyAppWebViewClient());
         webView.loadUrl("https://winjaa.net/");
+    }
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
